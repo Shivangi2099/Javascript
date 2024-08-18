6f935c63d4f98f56aa66d58e4b0a70f9086dd64b
Shivangi2099 <84871000+Shivangi2099@users.noreply.github.com>
2024-08-18 02:08:50 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score ="shivangi"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true =>1; false =>0
+
+let isLoggedIn ="Shivangi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0=> false
+//"" =>false
+// "shivangi" => true
+// if empty then false
+
+let someNumber =33
+let stringNumber= String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ***********Operations ***********
+let value = 3
+let negValue = -3
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " shivangi"
+
+let str3 = str1+str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2+ "2");
+
+
+//console.log(true);
+
+//**************comparision******* */
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === compare value and datatype
+console.log("2" === 2);
