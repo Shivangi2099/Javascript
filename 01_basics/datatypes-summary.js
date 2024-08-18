9b8a3313b28c9655306d57d4dcf85e19408a4a83
Shivangi2099 <84871000+Shivangi2099@users.noreply.github.com>
2024-08-18 02:52:19 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive 
+// 7 types : string(string),Number(number),Boolean(boolean),null(Object),undefined(undefined),symbol(symbol),BigInt(bigin)
+
+const score = 100
+const scoreValue =100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+const bigNumber = 7654324356654325434323n
+
+
+
+
+
+
+
+// Reference types(Non primitive)
+
+// Array,Objects, Functions
+
+//Array   datatype- object
+const heros =["shaktiman","naagraj","doga"];
+
+//object   datatype-object
+let myObj ={
+    name:"shivangi",
+    age: 22,
+}
+
+
+//function
+//syntax--- function(){}, datatype- object function
+
+const myFunction = function(){
+    console.log("Hellow World");
+    
+}
+
+console.log(typeof isLoggedIn);
